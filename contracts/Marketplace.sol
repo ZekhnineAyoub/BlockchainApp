534da4429253e00203912d4b0147a0e50cdcae60
ZEKHNINE Ayoub <ayoub.zekhnine@efs.sante.fr>
2022-01-08 01:49:32 +0100

    start consomation abi

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -11,7 +11,7 @@ contract Marketplace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -41,7 +41,30 @@ contract Marketplace {
 
       products[productCount]= Product(productCount,_name,_price,msg.sender,false);
 
-      emit ProductCreated((productCount,_name,_price,msg.sender,false);
+    }
+
+    function purshaseProduct(uint _id) public payable{
+        //fetch the product 
+
+        Product memory _product  = products[_id];
+        //fetch the owner 
+        address  payable _seller = _product.owner;
+        //make sure the product is valid
+        require(_product.id>0 &&  _product.id<= productCount);
+        //require that there is enough ether in the transaction
+        require(msg.value>= _product.price);
+        //require that product has not been purchased already
+        require(!_product.purchased);
+        //require that the buyer is not the seller
+        require(_seller!=msg.sender);
+        //trasfer owenership to the buyer
+        _product.owner = msg.sender;
+        //Mark as purchased 
+        _product.purchased = true;
+        //update the product
+        products[_id]= _product;
+        //Pay the seller by sending them ether
+        address(_seller).transfer(msg.value);
 
     }
 
